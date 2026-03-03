10777fa0472ca063026f74ce9ecc863fae7898aa
winter <dev.kushal@icloud.com>
2026-03-03 21:36:08 +0530

    dev.to link

--- src/utils/readTime.ts
+++ src/utils/readTime.ts
@@ -1,5 +1,5 @@
 
-export const calculateReadingTime = (wordCount: number, wordsPerMinute = 225) => {
+export const calculateReadingTime = (wordCount: number, wordsPerMinute = 175) => {
   if (wordCount === 0) return 0
   return Math.max(1, Math.ceil(wordCount / wordsPerMinute))
 }
